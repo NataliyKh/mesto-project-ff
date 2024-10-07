2a9ed397d7ed1711894ac4eb19d03a2036876014
Nataly Khramova <khramovanataliya@yandex.ru>
2024-10-07 23:44:34 +0300

    iter 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteFunction) {
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (initialCard) {
+    let newCard = createCard(initialCard, function () {
+        alert('hello')
+    }); 
+    
+    placesList.append(newCard);
+});
